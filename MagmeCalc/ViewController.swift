7ff541a58f14d7509ceec321c85911100f0de671
MagmMe <marcin@magm.me>
2019-11-05 05:47:04 +0100

    Initial Commit

---
+++ MagmeCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagmeCalc
+//
+//  Created by Marcin Magiera on 05/11/2019.
+//  Copyright © 2019 Magme Agency. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
