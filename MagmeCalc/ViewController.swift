0cfa82b9a3daccb04fe2e7f4ad7d5621a09ed22d
MagmMe <marcin@magm.me>
2019-11-07 13:52:01 +0100

    Creating simpel calc wit Swift 5

--- MagmeCalc/ViewController.swift
+++ MagmeCalc/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOne: UITextField!
+    @IBOutlet weak var numberTwo: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func sumAction(_ sender: Any) {
+        
+        if  let firstNumber = Int(numberOne.text!){
+            if let  secondNumber = Int(numberTwo.text!){
+                
+                let sumPlus = firstNumber + secondNumber
+                result.text = String(sumPlus)
+            }
+            
+        }
+        
+    }
+    
+    
+    @IBAction func minusAction(_ sender: Any) {
+       if  let firstNumber = Int(numberOne.text!){
+           if let  secondNumber = Int(numberTwo.text!){
+               
+               let sumPlus = firstNumber - secondNumber
+               result.text = String(sumPlus)
+           }
+           
+       }
+       
+    }
+    
+    @IBAction func multiplyAction(_ sender: Any) {
+       if  let firstNumber = Int(numberOne.text!){
+           if let  secondNumber = Int(numberTwo.text!){
+               
+               let sumPlus = firstNumber * secondNumber
+               result.text = String(sumPlus)
+           }
+           
+       }
+       
+    }
+    @IBAction func divideAction(_ sender: Any) {
+        if  let firstNumber = Int(numberOne.text!){
+            if let  secondNumber = Int(numberTwo.text!){
+                
+                let sumPlus = firstNumber / secondNumber
+                result.text = String(sumPlus)
+            }
+            
+        }
+        
+    }
+    
 }
 
